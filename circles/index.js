ca7935846b4028bd875e43ce423131fa19a7771c
Andy Nguyen <apnguyen11@yahoo.com>
2019-11-05 20:33:42 -0600

    generated random circles

--- circles/index.js
+++ circles/index.js
@@ -7,3 +7,5 @@ dispatch({ type: null }); // Here we're making a call to dispatch() - this trigg
 
 // Write DOM event listeners here, make them dispatch actions to the Redux store 
 
+const addBtn = document.getElementById("addCircle")
+addBtn.addEventListener('click', e => dispatch({ type: "ADDCIRCLE" }));
--- circles/reducer.js
+++ circles/reducer.js
@@ -17,11 +17,31 @@ const initialState = [
     },
 ];
 
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+
 const reducer = (state = initialState, action) => {
     // Handle actions here - make sure you don't mutate the state!
     const { type } = action;
-
+    console.log(action)
     // ACTION: Add a random circle
+    if(type === "ADDCIRCLE"){
+        return [...state, { radius: Math.floor(Math.random() * 80),
+            color: `${getRandomColor()}`} ]
+        // return{
+            // radius: Math.floor(Math.random() * 80),
+            // color: `${getRandomColor()}`
+        // }
+
+        console.log([...state, "hi"]);
+    }
 
     return state;
 }
