233afe84f3a3e199bb4d3f9a741f034be6e901b6
Andy Nguyen <apnguyen11@yahoo.com>
2019-11-06 18:19:41 -0600

    WIP rectangle excercise. changing size and color of rectangle

--- rectangle/index.js
+++ rectangle/index.js
@@ -7,3 +7,11 @@ dispatch({type: null}); // Here we're making a call to dispatch() - this trigger
 
 // Write DOM event listeners here, make them dispatch actions to the Redux store 
 
+var plus10h = document.getElementById("height");
+plus10h.addEventListener("click", e => dispatch({type: "HEIGHT"}))
+var plus10w = document.getElementById("width");
+plus10w.addEventListener("click", e => dispatch({type: "WIDTH"}))
+var redder = document.getElementById("red");
+redder.addEventListener("click", e => dispatch({type: "RED"}))
+var bluer = document.getElementById("blue");
+bluer.addEventListener("click", e => dispatch({type: "BLUE"}))
--- rectangle/reducer.js
+++ rectangle/reducer.js
@@ -9,10 +9,37 @@ const reducer = (state = initialState, action) => {
     const { type } = action;
 
     // ACTION 1 - Increment height by 10
-
+    if(type === "HEIGHT"){
+        return {
+            height: state.height + 10,
+            width: state.width,
+            color: state.color
+        }
+    }
     // ACTION 2 - Increment width by 10
-
+    if(type === "WIDTH"){
+        return {
+            height: state.height,
+            width: state.width + 10,
+            color: state.color
+        }
+    }
     // ACTION 3 - Change the color
 
+    if(type === "RED"){
+        return {
+            height: state.height,
+            width: state.width,
+            color: state.color = "red"
+        }
+    }
+
+    if(type === "BLUE"){
+        return {
+            height: state.height,
+            width: state.width,
+            color: state.color = "blue"
+        }
+    }
     return state;
 }
